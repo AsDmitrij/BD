3e8307822c05ad453887e5617c9f9fa204fa6adb
AsDmitrij <asdmitrij@mail.ru>
2018-05-25 13:16:57 +0300

    Delete 3nfConstructorsCountry.sql

--- Lab4/3nfConstructorsCountry.sql
+++
@@ -1,9 +0,0 @@
-CREATE 
-    ALGORITHM = UNDEFINED 
-    DEFINER = `asdmitrij`@`%` 
-    SQL SECURITY DEFINER
-VIEW `lab_4`.`3nfconstructorscountry` AS
-    SELECT DISTINCT
-        `2nfc`.`Name` AS `Name`, `2nfc`.`Country` AS `Country`
-    FROM
-        `lab_4`.`2nfconstructors` `2nfc`
